b0c1457362b1f8b3b4c02b3cb0c219866a8a8dc8
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-02-13 09:37:36 +0530

    Create 1_print_fibo_series.java

---
+++ 1_Recursion/1_print_fibo_series.java
@@ -0,0 +1,19 @@
+public class Main{
+    
+    
+    public static void fibo(int terms, int a, int b){
+        
+        if(terms == 0)
+            return;
+        
+        System.out.print(a + " ");
+        fibo(terms-1, b, a+b);
+    }
+    
+    
+    public static void main(String args[]){
+        int n = 13;
+        fibo(n, 0, 1);
+    }
+    
+}
